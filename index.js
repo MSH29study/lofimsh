78beddeec74a858259ab3645dfebb7b79e0a7184
MSH29study <168229068+MSH29study@users.noreply.github.com>
2024-04-28 18:48:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 const channelId = '1229892286691938365';
 const guildId = '1157418594649522268'; 
-const Url = 'https://youtu.be/MT8mUsrShEk?si=bYdNushQauLVIJ4a'; 
+const Url = 'https://youtu.be/ZmDBbnmKpqQ?si=iRMXqcGGCc_H1DUA'; 
 
 client.on('ready', () => {
   console.log(`✅ | Logged in as ${client.user.tag}`);
