618a8c15d2ba53e41728677a4c63f72a01744b86
MSH29study <168229068+MSH29study@users.noreply.github.com>
2024-04-28 19:26:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
 const play = require('play-dl'); 
 require('dotenv').config();
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
-});
 const express = require("express")
 const app = express();
-var listener = app.listen(process.env.PORT || 2000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+
 app.listen(() => console.log("I'm Ready To Work..! 24H"));
 app.get('/', (req, res) => {
   res.send(`
@@ -18,35 +12,24 @@ app.get('/', (req, res) => {
   <center><h1>Bot 24H ON!</h1></center
   </body>`)
 });
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
 const channelId = '1229892286691938365';
 const guildId = '1157418594649522268'; 
-const Url = 'https://youtu.be/ZmDBbnmKpqQ?si=iRMXqcGGCc_H1DUA'; 
+const Url = 'https://www.youtube.com/watch?v=eXp4Mt1S8Lg&list=PLcetZ6gSk96-FECmH9l7Vlx5VDigvgZpt&index='; 
 
 client.on('ready', () => {
   console.log(`✅ | Logged in as ${client.user.tag}`);
-  joinAndPlayQuran(guildId, channelId);
+  joinAndPlayPlaylist(guildId, channelId);
 });
 
-async function playQuran(channel, message) {
-  const player = createAudioPlayer();
-  const connection = joinVoiceChannel({
-    channelId: channel.id,
-    guildId: channel.guild.id,
-    adapterCreator: channel.guild.voiceAdapterCreator,
-  });
-
-  connection.subscribe(player);
-
-  const stream = await play.stream(Url);
-  const resource = createAudioResource(stream.stream, { inputType: stream.type });
-  player.play(resource);
-  player.on(AudioPlayerStatus.Playing, () => console.log('Playing Podcast'));
-  
-
-  player.on('error', error => console.error(`Error: ${error.message}`));
-}
-
-async function joinAndPlayQuran(guildId, channelId) {
+async function joinAndPlayPlaylist(guildId, channelId) {
   const channel = await client.channels.fetch(channelId);
   const connection = joinVoiceChannel({
     channelId: channel.id,
@@ -56,14 +39,14 @@ async function joinAndPlayQuran(guildId, channelId) {
 
   const player = createAudioPlayer();
 
-  const stream = await play.stream(Url);
-  const resource = createAudioResource(stream.stream, { inputType: stream.type });
-
-  player.play(resource);
-  connection.subscribe(player);
-
-  player.on(AudioPlayerStatus.Playing, () => console.log('Playing Quran'));
-  player.on('error', error => console.error(`Error: ${error.message}`));
+  for(var i = 1; i < 343 ;i++){
+    const stream = await play.stream(Url+i);
+    const resource = createAudioResource(stream.stream, { inputType: stream.type });
+    player.play(resource);
+    connection.subscribe(player);
+    player.on(AudioPlayerStatus.Playing, () => console.log('Playing Podcast Platylist'));
+    player.on('error', error => console.error(`Error: ${error.message}`));
+  }
 }
 
 
